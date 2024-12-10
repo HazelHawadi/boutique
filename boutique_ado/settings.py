ee6672483914dee6eb3005f665591bba3a5a479f
Hazel Hawadi <hawadihazel@icloud.com>
2024-12-10 12:43:58 +0000

    added mobile_header and main navbar

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -153,4 +153,3 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
